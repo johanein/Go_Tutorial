ca8d87b36f25afc06cd4eefffbdaad0443e70c68
albert.y <albert.y@justdial.com>
2022-04-26 19:10:42 +0530

    module tutorial till greeting multiple people

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"rsc.io/quote/v4"
 
@@ -11,7 +12,40 @@ import (
 func main() {
 	fmt.Println("Hello december")
 	fmt.Println(quote.Go())
-	// Get a greeting message and print it.
-	message := goTutorialGreetings.Hello("Gladys")
+
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request goTutorialGreetings messages for the names.
+	messages0, err0 := goTutorialGreetings.Hellos(names)
+	if err0 != nil {
+		log.Fatal(err0)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages0)
+
+	// Get a goTutorialGreetings message and print it.
+	message, err := goTutorialGreetings.Hello("Albert")
+	// If no error was returned, print the returned message
+	// to the console.
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Get a goTutorialGreetings message and print it.
 	fmt.Println(message)
+	message1, err1 := goTutorialGreetings.Hello("")
+	// If no error was returned, print the returned message
+	// to the console.
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+	// Get a goTutorialGreetings message and print it.
+	fmt.Println(message1)
 }
